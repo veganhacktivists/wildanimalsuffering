7d849f130ab51baa12743f28d16b7c3b2d7e1b5e
John Fisher <jgf5013@gmail.com>
2022-04-04 22:35:14 +0200

    task(resources): adding in some basic images

---
+++ resources/assets/js/app.js
@@ -0,0 +1,12 @@
+// var firstsec = document.getElementById('f');
+// var secondsec = document.getElementById('s');
+// var lastScrollTop = 0;
+// window.onscroll = function () {
+//     var st = window.pageYOffset || document.documentElement.scrollTop;
+//     if (st > lastScrollTop) {
+//         secondsec.scrollIntoView({ behavior: "smooth" });
+//     } else {
+//         firstsec.scrollIntoView({ behavior: "smooth" });
+//     }
+//     lastScrollTop = st <= 0 ? 0 : st;
+// };
