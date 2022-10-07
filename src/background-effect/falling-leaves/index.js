86c3e4af755acf5f4ffca3986a6365a4dee37a47
Jérémy Touati <sielver@gmail.com>
2022-10-07 20:19:54 +0200

    Create falling leaves background effect component

---
+++ src/background-effect/falling-leaves/index.js
@@ -0,0 +1,35 @@
+import './falling-leaves.css';
+
+const FallingLeavesEffect = () => {
+	return (
+		<>
+			<div
+				style={{
+					position: 'fixed',
+					width: '100vw',
+					height: '100vh',
+				}}
+			>
+				<div id="leaves">
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+					<i></i>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export { FallingLeavesEffect };
