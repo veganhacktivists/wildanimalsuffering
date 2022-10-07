5184928a3e1ee1786c76d79a1af9959415f60205
Jérémy Touati <sielver@gmail.com>
2022-10-07 15:35:44 +0200

    Show rainfall effect on "even worse" screen

--- src/background-effect/index.js
+++ src/background-effect/index.js
@@ -1,14 +1,18 @@
 import { useContext } from 'react';
 import { Context } from '../state';
+import { RainfallEffect } from './rainfall';
 import { SnowfallEffect } from './snowfall';
 
 const BackgroundEffect = () => {
 	const [state] = useContext(Context);
 
 	switch (state.screenId) {
+	case 'even-worse-screen':
+		return <RainfallEffect />;
 	case 'videos-screen':
 		return <SnowfallEffect />;
 	}
+
 };
 
 export { BackgroundEffect };
