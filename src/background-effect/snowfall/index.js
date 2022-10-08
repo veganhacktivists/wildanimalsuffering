16549d9277f7b248d5b7425bcf5c5f28aec0056b
Jérémy Touati <sielver@gmail.com>
2022-10-08 10:27:13 +0200

    Use tailwind classes when possible

--- src/background-effect/snowfall/index.js
+++ src/background-effect/snowfall/index.js
@@ -2,15 +2,13 @@ import Snowfall from 'react-snowfall';
 
 const SnowfallEffect = () => {
 	return (
-		<>
-			<Snowfall
-				style={{
-					position: 'fixed',
-					width: '100vw',
-					height: '100vh',
-				}}
-			/>
-		</>
+		<Snowfall
+			style={{
+				position: 'fixed',
+				width: '100vw',
+				height: '100vh',
+			}}
+		/>
 	);
 };
 
