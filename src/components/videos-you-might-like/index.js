99d61a70293bc77680452acda55b50f627ed55f6
Jérémy Touati <sielver@gmail.com>
2022-10-14 20:20:26 +0200

    Update page

--- src/components/videos-you-might-like/index.js
+++ src/components/videos-you-might-like/index.js
@@ -1,44 +1,97 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "../../state";
+import { YoutubeVideo } from "./youtube-video";
+import { VideoPreview } from "./video-preview";
+import classNames from "classnames";
+import videos from "./videos";
 
 import "./videos-you-might-like.css";
 
 const VideosYouMightLike = () => {
-  const [state, dispatch] = useContext(Context);
+  const [state] = useContext(Context);
   const { translations, locale } = state;
   const lang = translations[locale];
+
+  const [videoPlayers, setVideoPlayers] = useState({});
+  const [currentVideoId, setCurrentVideoId] = useState(null);
+
+  const onReady = function (videoId, event) {
+    setVideoPlayers({ ...videoPlayers, [videoId]: event.target });
+  };
+
+  const stopAll = function () {
+    for (const videoPlayer of Object.values(videoPlayers)) {
+      videoPlayer.stopVideo();
+    }
+  };
+
+  const play = function (videoId) {
+    setCurrentVideoId(videoId);
+    stopAll();
+    videoPlayers[videoId].playVideo();
+  };
 
   return (
     <>
       <section
         id="videos-screen"
         data-name="Videos"
-        className="videos screen-content"
+        className="videos screen-content box-border !pb-32"
       >
         <div className="full-screen flex-col w-full h-full">
           <div className="py-16 flex justify-center split-screen flex-col h-screen-50vh w-full text-center items-center">
             <h1>{lang["videos-you-might-like"]}</h1>
           </div>
-          <div className="split-screen h-screen-50vh w-full flex-col relative">
-            <div className="flex flex-row">
-              <div className="main-video-container split-screen-padding md:w-2/3 flex flex-col justify-center items-center flex-grow-1">
-                <div className="w-full h-full flex-grow-1">
-                  {/* <iframe className="h-full m-auto" src="https://www.youtube.com/embed/1Dp6gObE9eA" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> */}
-                </div>
-                {/* putting padding here to get the width to match that of the video is pretty hacky :( */}
-                <p className="py-2 text-center">{lang["video-caption"]}</p>
+
+          <div className="grid grid-cols-12 gap-8 mx-8 xl:mx-32 2xl:mx-96 h-full overflow-hidden p-2">
+            <div className="col-span-9 h-full">
+              {videos.map((video) => {
+                return (
+                  <YoutubeVideo
+                    key={video.id}
+                    videoId={video.id}
+                    className={classNames("h-[80%]", {
+                      hidden: video.id !== currentVideoId,
+                    })}
+                    onReady={onReady}
+                  />
+                );
+              })}
+
+              {currentVideoId === null && (
+                <VideoPreview
+                  key={videos[0].id}
+                  video={videos[0]}
+                  className="h-[80%] p-5 rounded-t-xl"
+                  playBtnWrapperClassName="flex grow items-center justify-center mt-12"
+                  titleWrapperClassName="px-6 py-3"
+                  authorClassName="text-lg"
+                  titleClassName="text-xl uppercase"
+                  onClick={play}
+                />
+              )}
+
+              <div className="rounded-b-xl bg-khaki text-black h-[20%] p-4 text-lg shadow-[0px_0px_3px_1px_rgba(255,255,255,1)]">
+                {lang["video-caption"]}
               </div>
-              <div className="video-thumbnail-container split-screen-padding md:w-1/3 flex flex-col justify-center flex-grow-0">
-                <div>
-                  {/* <iframe src="https://www.youtube.com/embed/yMtwbur0ylo" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
-                </div>
-                <div>
-                  {/* <iframe src="https://www.youtube.com/embed/OYJi9zLBjaI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
-                </div>
-                <div>
-                  {/* <iframe src="https://www.youtube.com/embed/1VYYHLH990A" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe> */}
-                </div>
-              </div>
+            </div>
+
+            <div className="col-span-3 h-full space-y-6 overflow-y-scroll p-2 py-[3px] pr-12">
+              {videos.map((video) => {
+                return (
+                  <VideoPreview
+                    key={video.id}
+                    video={video}
+                    className="h-[31%] p-3 rounded-xl"
+                    playBtnWrapperClassName="flex grow items-center mt-12"
+                    playBtnClassName="w-12 h-12"
+                    titleWrapperClassName="px-2 py-1"
+                    authorClassName="text-xs"
+                    titleClassName="text-sm"
+                    onClick={play}
+                  />
+                );
+              })}
             </div>
           </div>
         </div>
