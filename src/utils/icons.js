661e67564121ca8cf9716979c59e01fe78f0c68d
Jérémy Touati <sielver@gmail.com>
2022-10-14 20:00:13 +0200

    Add new SVG icon for video play action

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -3,12 +3,14 @@ import github from "../../assets/icons/social/github.svg";
 import instagram from "../../assets/icons/social/instagram.svg";
 import patreon from "../../assets/icons/social/patreon.svg";
 import youtube from "../../assets/icons/social/youtube.svg";
+import videoPlay from "../../assets/icons/video-play.svg";
 
 const ICONS = {
   GITHUB: github,
   INSTAGRAM: instagram,
   PATREON: patreon,
   YOUTUBE: youtube,
+  VIDEO_PLAY: videoPlay,
 };
 
 export default ICONS;
