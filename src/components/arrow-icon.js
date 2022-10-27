add092491f275659ac4fb88f9fe4610cecc2376b
Stephan de Vries <stephan281094@gmail.com>
2022-10-27 21:35:28 +0200

    Add common objections screen

---
+++ src/components/arrow-icon.js
@@ -0,0 +1,18 @@
+export function ArrowIcon({ size, ...props }) {
+  return (
+    <svg
+      height={size}
+      viewBox="0 0 32 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M29.8574 16.3789L16.0003 2.99966L2.14327 16.3789"
+        stroke="#4e4534"
+        strokeWidth="3"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
--- src/screens/population-dynamics/index.js
+++ src/screens/population-dynamics/index.js
@@ -8,12 +8,13 @@ export function PopulationDynamics() {
     offset: ["end 2", "end start"],
   });
   const chickOpacity = useTransform(scrollYProgress, [0.4, 0.6], [0.05, 0.6]);
+  const conclusionOpacity = useTransform(scrollYProgress, [0.5, 0.7], [0, 1]);
 
   return (
     <section
       id="population-dynamics"
       ref={scrollRef}
-      className="relative min-h-screen bg-dirt-road bg-cover bg-center py-24"
+      className="relative flex min-h-screen flex-col bg-dirt-road bg-cover bg-center py-24"
     >
       <img
         className="absolute top-0 left-0"
@@ -87,18 +88,21 @@ export function PopulationDynamics() {
       </div>
       <div className="relative mx-auto flex w-full max-w-7xl flex-col px-10">
         <div className="grid grid-cols-4">
-          <div className="col-span-2 col-start-2 space-y-10">
+          <div className="col-span-2 col-start-2 flex h-full flex-col space-y-10">
             <h2 className="text-center font-brand text-4xl text-white">
               Population Dynamics
             </h2>
-            <p className="mx-auto max-w-lg text-center text-lg text-white">
+            <p className="mx-auto max-w-lg flex-1 text-center text-lg text-white">
               Wild animals tend to have a lot of children, and for populations
               to remain stable, on average each parent can only have 2 children
-              that reach adulthood.{" "}
-              <span className="font-bold">
-                This means that most animals die young.
-              </span>
+              that reach adulthood.
             </p>
+            <motion.p
+              className="mx-auto mt-auto text-lg font-bold text-white"
+              style={{ opacity: conclusionOpacity }}
+            >
+              This means that most animals die young.
+            </motion.p>
           </div>
           <img src="/images/population-dynamics/duck.png" alt="" />
         </div>
