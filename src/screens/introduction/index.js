81947c2a2c48add1e6d133a25df637e94090f766
trybick <t.rybicki@gmail.com>
2022-11-14 13:17:36 -0500

    Revert introduction changes

--- src/screens/introduction/index.js
+++ src/screens/introduction/index.js
@@ -13,40 +13,37 @@ export function Introduction() {
               className="mx-auto w-3/4 md:w-1/2 lg:w-full"
             />
           </div>
-          <div className="flex flex-col space-y-10 pt-12">
-            <h1 className="flex max-w-md flex-col space-y-4 font-brand">
-              <span className="text-6xl leading-none text-white">
-                Wild Animal Suffering
-              </span>
-              <span className="text-3xl text-primary">
-                ...and why it matters
-              </span>
-            </h1>
-            <div className="grid grid-cols-2 gap-10 font-sans text-lg font-light text-white">
-              <p>
-                We tend to idealize nature and the lives of wild animals, but in
-                reality, their lives can be really tough..{" "}
-                <span className="font-medium">
-                  They have to deal with harsh weather conditions, hunger,
-                  thirst, disease, violence from other animals and more.
+          <div className="bg-gradient-to-b from-transparent via-black to-black lg:bg-none lg:pt-12">
+            <div className="flex flex-col space-y-10 px-10 py-10 lg:py-0">
+              <h1 className="flex flex-col space-y-4 text-center font-brand lg:max-w-md lg:text-left">
+                <span className="text-5xl leading-none text-white lg:text-6xl">
+                  Wild Animal Suffering
                 </span>
-              </p>
-              <p>
-                When we suffer, we seek relief. For animals, their suffering
-                doesn&apos;t become any less unpleasant because they live in the
-                wild, and pain feels the same for them, regardless of whether or
-                not{" "}
-                <span className="font-medium">humans are the cause of it.</span>
-              </p>
+                <span className="text-2xl text-primary lg:text-3xl">
+                  ...and why it matters
+                </span>
+              </h1>
+              <div className="grid gap-10 text-lg text-white md:grid-cols-2">
+                <p>
+                  We tend to idealize nature and the lives of wild animals, but
+                  in reality, their lives can be really tough.. They have to
+                  deal with harsh weather conditions, hunger, thirst, disease,
+                  violence from other animals and more.
+                </p>
+                <p>
+                  When we suffer, we seek relief. For animals, their suffering
+                  doesn&apos;t become any less unpleasant because they live in
+                  the wild, and pain feels the same for them, regardless of
+                  whether or not humans are the cause of it.
+                </p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="mt-14">
-          <p className="text-center text-lg font-bold text-white">
-            The suffering of wild animals matters. It matters to them, and it
-            should matter to us.
-          </p>
-        </div>
+        <p className="bg-black px-10 pb-10 text-center text-lg font-bold text-white lg:bg-transparent">
+          The suffering of wild animals matters. It matters to them, and it
+          should matter to us.
+        </p>
       </div>
     </section>
   );
