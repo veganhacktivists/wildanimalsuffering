4d70b7d7fbd27afa4c671bd293c176b78f50f4fd
Gerard O'Neill <GRardB@users.noreply.github.com>
2022-11-14 15:34:55 +0100

    Temporarily remove the navigation menu from the header (#36)

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,7 @@
-import { Navigation } from "./navigation";
-
 export function Header() {
   return (
     <header className="absolute left-0 z-10 flex w-full items-start p-10 text-gray">
       <div className="flex flex-1 items-center">
-        <Navigation />
         <span className="ml-10 hidden font-light md:block">
           Illustrations by{" "}
           <a
