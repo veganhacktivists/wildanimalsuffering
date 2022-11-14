e3233764f360518b3c2123523e798268eb895f6b
Gerard O'Neill <grardb@gmail.com>
2022-11-14 17:09:24 +0100

    Remove left margin in header

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header className="absolute left-0 z-10 flex w-full items-start p-10 text-gray">
       <div className="flex flex-1 items-center">
-        <span className="ml-10 hidden font-light md:block">
+        <span className="hidden font-light md:block">
           Illustrations by{" "}
           <a
             className="font-normal underline"
