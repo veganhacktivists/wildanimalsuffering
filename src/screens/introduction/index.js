d6561e0da9ec7d493848c3c68f1402a20156a7d0
Stephan de Vries <stephan281094@gmail.com>
2022-11-19 18:05:10 +0100

    Update vh logo + remove constants

--- src/screens/introduction/index.js
+++ src/screens/introduction/index.js
@@ -4,11 +4,14 @@ export function Introduction() {
       id="introduction"
       className="flex min-h-screen items-end bg-black bg-savanna bg-[length:100%] bg-[center_-100px] bg-no-repeat lg:items-center lg:bg-savanna-md lg:bg-cover lg:bg-center"
     >
-      <img
-        className="absolute top-0 right-0 m-10 w-20"
-        src="/images/logo-vh.svg"
-        alt="Vegan Hacktivists Logo"
-      />
+      <a
+        className="absolute top-0 right-0 m-10 hidden w-20 opacity-75 hover:opacity-100 lg:block"
+        href="https://veganhacktivists.org/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src="/images/logo-vh.svg" alt="Vegan Hacktivists Logo" />
+      </a>
 
       <div className="mx-auto flex w-full max-w-7xl flex-col lg:gap-20 xl:gap-0">
         <div className="grid lg:grid-cols-2">
