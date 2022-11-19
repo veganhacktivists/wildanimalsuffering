480679f763034bb278778b572a602cfc48ce2dcf
Stephan de Vries <stephan281094@gmail.com>
2022-11-19 20:54:39 +0100

    Add scroll indicator to introduction screen

---
+++ src/components/scroll-down-indicator.js
@@ -0,0 +1,9 @@
+export function ScrollDownIndicator() {
+  return (
+    <div className="py-6">
+      <span className="-mt-2 block h-4 w-4 rotate-45 animate-arrows-down border-b-2 border-r-2 border-white" />
+      <span className="-mt-2 block h-4 w-4 rotate-45 animate-arrows-down border-b-2 border-r-2 border-white -animation-delay-200" />
+      <span className="-animation-delay-400 -mt-2 block h-4 w-4 rotate-45 animate-arrows-down border-b-2 border-r-2 border-white" />
+    </div>
+  );
+}
