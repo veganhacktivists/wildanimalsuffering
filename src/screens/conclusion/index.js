ea314845c87bfb322ac6b907755e9913b7fb34d7
Tim Rybicki <39889198+trybick@users.noreply.github.com>
2022-11-19 10:51:07 -0500

    Style Conclusion page part 2 (#44)
    * Base
    
    * Respn
    
    * PR feedback

--- src/screens/conclusion/index.js
+++ src/screens/conclusion/index.js
@@ -1,4 +1,5 @@
-import { PatreonIcon } from "../../components/patreon-icon";
+import { HealthcareIcon } from "../../components/healthcare-icon";
+import { links } from "../../utils/constants";
 
 export function Conclusion() {
   return (
@@ -10,11 +11,6 @@ export function Conclusion() {
         className="not-sr-only absolute right-32 top-52 max-w-lg lg:w-full"
         src="/images/conclusion/momma.png"
         alt="Mother bear"
-      />
-      <img
-        className="not-sr-only absolute bottom-72 right-96 max-w-lg lg:w-full"
-        src="/images/conclusion/pups.png"
-        alt="Bear pups"
       />
       <div className="py-30 relative flex max-w-3xl flex-col pt-36 pl-28 text-white">
         <h2 className="mb-4 font-brand text-5xl leading-relaxed">
@@ -33,7 +29,7 @@ export function Conclusion() {
           matters to them. And it should matter to us.
         </p>
         <br />
-        <p className="font-lg">
+        <p className="font-lg z-10">
           If it matters to you, then we encourage you to join us in our mission
           to make life better for wild animals. There is much to learn about the
           situation of wild animals. This page is just an introduction to the
@@ -42,45 +38,78 @@ export function Conclusion() {
           opportunity to make a difference. We hope you choose to take it.
         </p>
         <br />
-        <h3 className="mt-10 font-brand text-4xl text-primary">Thank you.</h3>
+        <h3 className="z-10 mt-10 font-brand text-4xl text-primary">
+          Thank you.
+        </h3>
       </div>
-      <div className="flex flex-row justify-evenly p-5">
-        <div className="flex flex-row">
-          <PatreonIcon fill="white" />
-          <div className="mx-4 flex-col">
-            <h5>Please consider supporting our work!</h5>
-            <div className="text-sm">
+
+      <div className="relative mt-24 flex flex-row justify-evenly bg-white bg-opacity-10 py-10 px-10">
+        <img
+          className="not-sr-only absolute right-1/4 top-[-230px] max-w-lg lg:w-full"
+          src="/images/conclusion/pups.png"
+          alt="Bear pups"
+        />
+        <div className="flex">
+          <HealthcareIcon className="my-auto" />
+          <div className="ml-8 flex-col">
+            <h4 className="mb-2 text-xl font-bold text-white">
+              Please consider supporting our work!
+            </h4>
+            <span className="text-md font-light text-white">
               This website would not be possible without your support. Thank
               you!
-            </div>
+            </span>
           </div>
         </div>
-        <span className="uppercase">Support Us</span>
+        <a
+          href={links.veganHacktivistsSupport}
+          target="_blank"
+          rel="noreferrer"
+          className="ml-10 self-center whitespace-nowrap rounded-xl bg-primary px-5 py-1 text-xl font-medium uppercase"
+        >
+          Support Us
+        </a>
       </div>
-      <div className="flex flex-row">
-        <h5 className="px-20">Credits and sources</h5>
-        <div>
-          <p>
-            Mauris convallis arcu amet, sollicitudin gravida gravida platea.
+
+      <div className="mx-auto flex max-w-screen-2xl p-14">
+        <div className="grid grid-cols-4 gap-10 text-white">
+          <h4 className="text-lg font-bold">Special thanks</h4>
+          <p className="col-span-3 leading-relaxed">
+            To our partners and advisors from Wild Animal Initiative, Animal
+            Ethics, Rethinking Priorities, and Jack Hancock for his
+            collaborative role. Kate Rodman for the wonderful illustrations.
+            This website would not have been possible without everyone’s
+            dedication and support!
           </p>
-          <p>
-            Velit rhoncus pellentesque feugiat aenean tincidunt. Diam tincidunt
-            aliquet mauris lorem.
-          </p>
-          <p>
-            Egestas scelerisque ac risus. Neque nibh tellus elementum vulputate
-            auctor
-          </p>
-          <p>Arcu amet, sollicitudin gravida gravida platea.</p>
-          <p>Rrhoncus pellentesque feugiat aenean tincidunt</p>
-          <p>
-            Diam tincidunt aliquet mauris lorem egestas scelerisque ac risus
-          </p>
-          <p>Neque nibh tellus elementum vulputate auctor.</p>
-          <p>
-            Mauris convallis arcu amet, sollicitudin gravida gravida platea.
-          </p>
+          <h4 className="text-lg font-bold">Credits and sources</h4>
+          <ol className="col-span-3 list-inside list-decimal leading-relaxed">
+            <li>
+              <a
+                className="underline"
+                href="https://reducing-suffering.org/how-many-wild-animals-are-there"
+                rel="noreferrer"
+                target="_blank"
+              >
+                How many wild animals are there?
+              </a>
+            </li>
+            {/* TODO: Replace placeholder text with real sources */}
+            <li>
+              Velit rhoncus pellentesque feugiat aenean tincidunt. Diam
+              tincidunt aliquet mauris lorem.
+            </li>
+            <li>
+              Egestas scelerisque ac risus. Neque nibh tellus elementum
+              vulputate auctor.
+            </li>
+            <li>Arcu amet, sollicitudin gravida gravida platea.</li>
+          </ol>
         </div>
+        <img
+          className="my-auto ml-14 max-h-40"
+          src="/images/conclusion/vh-logo.png"
+          alt="Vegan Hacktivists logo"
+        />
       </div>
     </section>
   );
