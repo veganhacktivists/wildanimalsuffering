2905a8265e83438f43c0a1774950ae06f83a6f6d
David van Beveren <7482732+davidvanbeveren@users.noreply.github.com>
2023-03-30 15:02:49 +0200

    Update i18n.ts
    Co-authored-by: Stephan de Vries <stephan281094@users.noreply.github.com>

--- i18n.ts
+++ i18n.ts
@@ -1,15 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import de from "./lang/de.json";
 import en from "./lang/en.json";
 import fr from "./lang/fr.json";
-import de from "./lang/de.json";
 
 export const defaultNS = "translation";
 export const resources = {
+  de: { [defaultNS]: de },
   en: { [defaultNS]: en },
   fr: { [defaultNS]: fr },
-  de: { [defaultNS]: de },
 } as const;
 
 i18n.use(initReactI18next).init({
