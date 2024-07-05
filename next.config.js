13ff59f43d8e974878a8ba50e17ce4d20f7381cf
Headscracher <t.frohme@all4-one.de>
2024-07-05 10:52:03 +0200

    Update dist directory

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withExportImages = require("next-export-optimize-images");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'dist',
   swcMinify: true,
   trailingSlash: true,
 };
