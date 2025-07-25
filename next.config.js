d9fbb866fd0243665e80c6da19906af2e6465f4b
Jérémy Touati <sielver@gmail.com>
2025-07-25 15:32:36 +0200

    Remove obsolete flag

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const withExportImages = require("next-export-optimize-images");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   trailingSlash: true,
 };
 
