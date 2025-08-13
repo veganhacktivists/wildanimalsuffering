dc5e56c73a1d4e59925f26ae97b1cebf7f348440
David van Beveren <7482732+davidvanbeveren@users.noreply.github.com>
2025-08-13 18:24:37 +0900

    fix logo sizes

--- screens/organizations/organization.js
+++ screens/organizations/organization.js
@@ -3,7 +3,7 @@ import { cx } from "~/utils/cx";
 export function Organization({ name, children, imageUrl, links }) {
   return (
     <div className="flex flex-col items-center justify-center gap-4 rounded-xl bg-white/20 p-6 text-white lg:flex-row lg:items-start xl:gap-5">
-      <img className="aspect-square lg:w-16 xl:w-24" src={imageUrl} alt="" />
+			<img className="h-24 w-24 object-contain md:h-24 md:w-24 lg:h-24 lg:w-24 xl:h-24 xl:w-24" src={imageUrl} alt="" />
 
       <div className="flex h-full flex-col items-center lg:items-start">
         <h3 className="text-xl font-bold">{name}</h3>
